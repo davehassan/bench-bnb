bd2bfc40ac9e36f7c89f81ac9c9f881722665822
App Academy Student <appacademy@Apps-Mac-mini-27.local>
2015-11-13 15:10:52 -0500

    Markers are added and removed properly

--- app/assets/javascripts/components/map.js.jsx
+++ app/assets/javascripts/components/map.js.jsx
@@ -1,20 +1,67 @@
 window.Map = React.createClass({
+  _onChange: function () {
+    this.clearMarkers();
+    this.addMarkers();
+  },
+
   render: function () {
     return (
       <div className="map" ref="map"></div>
     );
   },
 
+  clearMarkers: function () {
+    var benches = BenchStore.all();
+    var markers = [];
+    this.markers.forEach(function (marker) {
+      var lat = marker.position.lat(), lng = marker.position.lng();
+      var onMap = false;
+
+      benches.forEach(function (bench) {
+        if (bench.lat === lat && bench.lng === lng) {
+          onMap = true;
+        }
+      });
+
+      if (!onMap) {
+        marker.setMap(null);
+      } else {
+        markers.push(marker);
+      }
+
+      this.markers = markers;
+    });
+  },
+
+  makeMarker: function (bench) {
+    var latLng = { lat: bench.lat, lng: bench.lng };
+    var desc = bench.description;
+
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: this.map,
+      title: desc
+    });
+
+    return marker;
+  },
+
   addMarkers: function () {
     var benches = BenchStore.all();
+
     benches.forEach(function (bench) {
-      var latLng = { lat: bench.lat, lng: bench.lng };
-      var desc = bench.description;
-      var marker = new google.maps.Marker({
-        position: latLng,
-        map: this.map,
-        title: desc
+      var hasMarker = false;
+      this.markers.forEach(function (marker) {
+        var lat = marker.position.lat(), lng = marker.position.lng();
+        if (bench.lat === lat && bench.lng === lng) {
+          hasMarker = true;
+        }
       });
+
+      if (!hasMarker) {
+        var marker = this.makeMarker(bench);
+        this.markers.push(marker);
+      }
     }.bind(this));
   },
 
@@ -27,16 +74,22 @@ window.Map = React.createClass({
 
     this.map = new google.maps.Map(map, mapOptions);
     this.map.addListener('idle', function () {
-      var bounds = this.map.getBounds();
-      var northEast = bounds.getNorthEast();
-      var southWest = bounds.getSouthWest();
-      var boundsObj = {
-        northEast: { lat: northEast.lat(), lng: northEast.lng() },
-        southWest: { lat: southWest.lat(), lng: southWest.lng() }
-      };
 
-      ApiUtil.fetchBenches(boundsObj);
+      ApiUtil.fetchBenches(this.formatBounds());
     }.bind(this));
-    BenchStore.addChangeListener(this.addMarkers);
+    this.markers = [];
+    BenchStore.addChangeListener(this._onChange);
+  },
+
+  formatBounds: function () {
+    var bounds = this.map.getBounds();
+    var northEast = bounds.getNorthEast();
+    var southWest = bounds.getSouthWest();
+    var boundsObj = {
+      northEast: { lat: northEast.lat(), lng: northEast.lng() },
+      southWest: { lat: southWest.lat(), lng: southWest.lng() }
+    };
+
+    return boundsObj;
   }
 });
